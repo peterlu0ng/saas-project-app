3abdff00e54764fa8efb8c55a88977e80222f36c
Peter Luong <peter.luong678@gmail.com>
2024-06-24 13:53:21 +1000

    generate artifacts scaffold

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
   validates :title, presence: true
   validates :details, presence: true
+  has_many :artifacts, dependent: :destroy
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_24_012533) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_24_033533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artifacts", force: :cascade do |t|
+    t.string "name"
+    t.string "key"
+    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_artifacts_on_project_id"
+  end
 
   create_table "members", force: :cascade do |t|
     t.bigint "tenant_id"
@@ -64,6 +73,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_24_012533) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "artifacts", "projects"
   add_foreign_key "members", "tenants"
   add_foreign_key "members", "users"
   add_foreign_key "tenants", "tenants"
