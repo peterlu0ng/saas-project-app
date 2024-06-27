b4dac9b034fa9f4424a36878322682d5b492973c
Peter Luong <peter.luong678@gmail.com>
2024-06-27 14:28:39 +1000

    update projects listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_24_233836) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_27_035043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,6 +89,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_24_233836) do
     t.index ["tenant_id"], name: "index_tenants_on_tenant_id"
   end
 
+  create_table "tenants_users", id: false, force: :cascade do |t|
+    t.bigint "tenant_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["tenant_id", "user_id"], name: "index_tenants_users_on_tenant_id_and_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -97,6 +103,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_24_233836) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "tenant_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
