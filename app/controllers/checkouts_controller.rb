948c1710fcb2fc40c4d66c793518522402cbc145
Peter Luong <peter.luong678@gmail.com>
2024-07-01 11:09:47 +1000

    add payment to users account

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -17,5 +17,7 @@ class CheckoutsController < ApplicationController
   def success
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
     @line_items = Stripe::Checkout::Session.list_line_items(params[:session_id])
+    current_user.paid = true
+    current_user.save!
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_29_044213) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_01_010343) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -197,6 +197,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_29_044213) do
     t.integer "tenant_id"
     t.string "city"
     t.string "country"
+    t.boolean "paid", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
